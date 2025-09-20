02a85b9be1f2b09a3eb266027c57f8daf8ee94cd
art9440 <art9440@gmail.com>
2025-09-20 19:19:48 +0700

    lab02 is done

--- lab02/src/stackDepth.ts
+++ lab02/src/stackDepth.ts
@@ -1,5 +1,42 @@
 import { ReversePolishNotationActionDict } from "./rpn.ohm-bundle";
+import { SyntaxError } from "./index";
 
 export const rpnStackDepth = {
+      Program_seq(items) {
+    let out = 0;
+    let max = 0;
+
+    for (const item of items.children) {
+      
+      const tokNode = item.child(0);
+      const tok = tokNode.sourceString.trim();
+
+      if (tok === "+" || tok === "*") {
+        
+        if (out < 2) {
+          throw new SyntaxError(`stack underflow at '${tok}'`);
+        }
+        
+        out -= 1;
+      } else {
+        
+        const v = Number(tok);
+        if (!Number.isFinite(v)) {
+          throw new SyntaxError(`invalid literal: ${tok}`);
+        }
+        out += 1;
+        if (out > max) max = out;
+      }
+    }
+
+  
+    if (out !== 1) {
+      throw new SyntaxError(
+        `expression must reduce to a single value (stack size = ${out})`
+      );
+    }
+
+    return { max, out };
+  },
 } satisfies ReversePolishNotationActionDict<StackDepth>;
 export type StackDepth = {max: number, out: number};
