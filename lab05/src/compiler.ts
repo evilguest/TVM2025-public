d45adb70a0bb0169fe4fe8a8b509a2d7c1ca050c
art9440 <art9440@gmail.com>
2025-10-18 17:34:57 +0700

    lab05 is done

--- lab05/src/compiler.ts
+++ lab05/src/compiler.ts
@@ -4,7 +4,37 @@ import { buildOneFunctionModule, Fn } from "./emitHelper";
 const { i32, get_local} = C;
     
 export function getVariables(e: Expr): string[] {
-    throw "Not implemented";
+    const vars: string[] = [];
+  const seen = new Set<string>();
+
+  function visit(node: Expr) {
+    switch (node.kind) {
+      case "Ident":
+        if (!seen.has(node.name)) {
+          seen.add(node.name);
+          vars.push(node.name);
+        }
+        break;
+
+      case "Int":
+        break;
+
+      case "Neg":
+        visit(node.expr);
+        break;
+
+      case "Add":
+      case "Sub":
+      case "Mul":
+      case "Div":
+        visit(node.left);
+        visit(node.right);
+        break;
+    }
+  }
+
+  visit(e);
+  return vars;
 }
 
 export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<number>>
@@ -14,5 +44,31 @@ export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<nu
 }
 
 function wasm(e: Expr, args: string[]): Op<I32> {
-    throw "Not implemented";
+    switch (e.kind) {
+    case "Int":
+      return i32.const(e.value);
+
+    case "Ident": {
+      const idx = args.indexOf(e.name);
+      if (idx === -1)
+        throw new WebAssembly.RuntimeError(`Unknown variable ${e.name}`);
+      return get_local(i32, idx);
+    }
+
+    case "Neg":
+      return i32.sub(i32.const(0), wasm(e.expr, args));
+
+    case "Add":
+      return i32.add(wasm(e.left, args), wasm(e.right, args));
+
+    case "Sub":
+      return i32.sub(wasm(e.left, args), wasm(e.right, args));
+
+    case "Mul":
+      return i32.mul(wasm(e.left, args), wasm(e.right, args));
+
+    case "Div":
+      // i32.div_s выбрасывает исключение при делении на 0
+      return i32.div_s(wasm(e.left, args), wasm(e.right, args));
+  }
 }
